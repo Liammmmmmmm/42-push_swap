01ecc7a17e2319a6747df835df873520cdae959f
Liam Lefebvre <lilefebv@student.42lyon.fr>
2024-12-03 16:55:39 +0100

    most simple sorting implementation, working but don't pass test program idk why

--- srcs/sort.c
+++ srcs/sort.c
@@ -6,14 +6,39 @@
 /*   By: lilefebv <lilefebv@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 16:18:11 by lilefebv          #+#    #+#             */
-/*   Updated: 2024/12/03 16:25:13 by lilefebv         ###   ########lyon.fr   */
+/*   Updated: 2024/12/03 16:39:06 by lilefebv         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+int	min(int *stack)
+{
+	int i;
+	int min = 2147483647;
+
+	i = 0;
+	while (i < len_a(-1))
+	{
+		if (stack[i] < min)
+			min = stack[i];
+		i++;
+	}
+	return (min);
+}
+
 void	sort(int *stack_a, int *stack_b)
 {
-	if(stack_a && stack_b)
-		return ; // just here to remove compilation error
+	int minv;
+
+	while (len_a(-1) > 0)
+	{
+		minv = min(stack_a);
+		while (stack_a[pos_a(-1)] != minv)
+			ra();
+		pb(stack_a, stack_b);
+	}
+	while (len_b(-1) > 0){
+		pa(stack_a, stack_b);
+	}
 }
